858b8b29d7e7be955afd2f9455768559a59be7f0
Daniel <dpericich@gmail.com>
2021-01-16 10:32:46 -0600

    inital commit and first steps on modular table

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+
+const players = [
+  {
+    team: "Kansas City Chiefs",
+    number: 87,
+    position: "Tight End",
+    name: "Travis Kelce",
+  },
+  {
+    team: "Kansas City Chiefs",
+    number: 10,
+    position: "Wide Receiver",
+    name: "Tyreek Hill",
+  },
+  {
+    team: "Kansas City Chiefs",
+    number: 15,
+    position: "QuarterBack",
+    name: "Patrick Mahomes",
+  },
+  {
+    team: "Kansas City Chiefs",
+    number: 95,
+    position: "Defensive Tackle",
+    name: "Chris Jones",
+  },
+];
 
 function App() {
+  const [sortedPlayers, setSortedPlayers] = useState(players);
+
+  console.log(sortedPlayers);
+
+  const sortByNumber = () => {
+    let sortedNumbers = sortedPlayers.sort((a, b) => a.number - b.number);
+    setSortedPlayers();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>Team</th>
+          <th onClick={sortByNumber}>Number</th>
+          <th>Position</th>
+          <th>Name</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedPlayers.map((player) => (
+          <tr key={`${player.number} ${player.name}`}>
+            <td>{player.team}</td>
+            <td>{player.number}</td>
+            <td>{player.position}</td>
+            <td>{player.name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
