3d94bd27bcc8557eba006493528f302467b44ed3
Daniel <dpericich@gmail.com>
2021-01-18 18:11:17 -0600

    changed styling and added in state callback function prop to dropdown component

--- src/components/FilterDropDown.js
+++ src/components/FilterDropDown.js
@@ -1,10 +1,8 @@
 import React from "react";
 
-const FilterDropDown = ({ items, value }) => {
+const FilterDropDown = ({ items, value, getValue }) => {
   // Create an empty array to store our unique items when iterate through our input
-  let uniqueItems = [];
-
-  console.log(value);
+  let uniqueItems = [""];
 
   // Loop over all items and if the value does not exist in the uniqueItems array, add it
   // to the array. Otherwise continue
@@ -13,16 +11,13 @@ const FilterDropDown = ({ items, value }) => {
       uniqueItems.push(item[value]);
     }
   });
+
   return (
     <div className="drop-down">
       <h2 className="drop-down__title">{value}</h2>
       <select
         className="drop-down__select"
-        onChange={() =>
-          console.log(
-            "I will hold the callback function to update the wrapper state"
-          )
-        }
+        onChange={(e) => getValue(e.target.value)}
       >
         {uniqueItems.map((item) => (
           <option className="drop-down__option">{item}</option>
